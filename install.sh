45d220fbdfa36fc795db1aefb6d0166a9039fba6
Raffaello Bonghi <raffaello@rnext.it>
2020-04-05 13:54:47 +0100

    Improve installer and add zed installation

--- install.sh
+++ install.sh
@@ -29,11 +29,12 @@ green=`tput setaf 2`
 reset=`tput sgr0`
 
 # Variable to show a message if is require a reboot
-REQUIRE_REBOOT=false
 DISTRO="melodic"
+ZED_VERSION="3.1"
 # Components
 ALL=false
 UDEV=false
+ZED=false
 ROS=false
 ROS_WS=false
 
@@ -77,6 +78,9 @@ ros_ws()
 
 ros()
 {
+
+    sudo apt-get install ros-melodic-robot
+
     # Add environment variables on bashrc
     if ! grep -Fxq "source /opt/ros/$DISTRO/setup.bash" $HOME/.bashrc ; then
         echo "   - Add ROS $DISTRO source to ${green}.bashrc${reset}"
@@ -95,6 +99,16 @@ ros()
     # TODO: Write at end install to reload bashrc
 }
 
+ros_status()
+{
+    if [ ! -z "ROS_DISTRO" ] ; then
+        echo $ROS_DISTRO
+        return
+    fi
+    # Return not installed
+    echo "NOT_INSTALLED"
+}
+
 
 versioning()
 {
@@ -111,13 +125,53 @@ versioning()
     echo "Versioning"
 }
 
+zed()
+{
+    local THIS="$(pwd)"
+    # Version link
+    # https://download.stereolabs.com/zedsdk/3.1/jp43/jetsons
+    local JP="jp${JETSON_JETPACK//./}"
+    local LINK="https://download.stereolabs.com/zedsdk/$ZED_VERSION/$JP/jetsons"
+    local DOWNLOAD_FILE="zed_driver_${ZED_VERSION//./}.run"
+    
+    echo " * Install ZED SDK $ZED_VERSION"
+    echo "   - Download from $LINK"
+
+    cd "$HOME/Downloads"
+    if [ ! -f $DOWNLOAD_FILE ] ; then
+        # Download ZED drivers
+        wget --output-document $DOWNLOAD_FILE $LINK
+        # Set executable launcher
+        chmod +x $DOWNLOAD_FILE
+        # Launch zed_driver in silent mode
+        ./$DOWNLOAD_FILE --quiet -- "silent"
+    fi
+    # Return to home folder
+    cd $THIS
+}
+
+
+zed_status()
+{
+    # Extract version
+    if [ -f "/usr/local/zed/zed-config-version.cmake" ] ; then
+        local version="$(cat //usr/local/zed/zed-config-version.cmake | head -1)"
+        version="$(echo $version | grep -oP 'PACKAGE_VERSION \"\K[^\"]+')"
+        echo "$version"
+        return
+    fi
+    # Return not installed
+    echo "NOT_INSTALLED"
+}
+
 
 udev()
 {
     local UDEV_FOLDER="/etc/udev/rules.d/"
     echo " * Setup UDEV"
-    echo "   - set dialout to $USER"
+    echo "   - set ${green}[dialout, input]{reset} to $USER"
     sudo adduser $USER dialout
+    sudo adduser $USER input
     echo "   - Install all rules in ${green}$UDEV_FOLDER${reset}"
     # https://unix.stackexchange.com/questions/66901/how-to-bind-usb-device-under-a-static-name
     sudo cp rules/* $UDEV_FOLDER
@@ -126,7 +180,9 @@ udev()
     sudo udevadm control --reload-rules
     sudo udevadm trigger
     # Require reboot
-    REQUIRE_REBOOT=true
+    if [ ! -f /var/run/reboot-required ] ; then
+        sudo touch /var/run/reboot-required
+    fi
 }
 
 
@@ -145,6 +201,7 @@ usage()
     echo "   -d|--distro    | Define ROS distribution Default:${green}$DISTRO${reset}"
     echo "   --all          | Install all parts"
     echo "   --udev         | Install UDEV rules"
+    echo "   --zed          | Install or update ZED drivers"
     echo "   --ros          | Install ROS ${green}$DISTRO${reset}"
     echo "   --ros-ws       | Install Panther ROS workspace"
 }
@@ -157,6 +214,9 @@ list_components()
     fi
     if $ROS || $ALL ; then
         echo " * ros"
+    fi
+    if $ZED || $ALL ; then
+        echo " * zed"
     fi
     if $ROS_WS || $ALL ; then
         echo " * ros-ws"
@@ -190,6 +250,10 @@ main()
                 UDEV=true
                 noflag=false
                 ;;
+            --zed)
+                ZED=true
+                noflag=false
+                ;;
             --ros)
                 ROS=true
                 noflag=false
@@ -217,13 +281,14 @@ main()
         usage
         exit 1
     fi
-
+    
     # Recap installatation
     echo "--- Board configuration ---"
     echo " - Hostname: ${green}$HOSTNAME${reset}"
     echo " - User: ${green}$USER${reset}"
     echo " - Home: ${green}$HOME${reset}"
-    echo " - ROS Distro: ${green}$DISTRO${reset}"
+    echo " - ZED version: ${green}$(zed_status)${reset} - $ZED_VERSION"
+    echo " - ROS Distro: ${green}$(ros_status)${reset} - $DISTRO"
     echo "---------------------------"
     echo " Installing list:"
     list_components
@@ -244,6 +309,10 @@ main()
     if $UDEV || $ALL ; then
         udev
     fi
+    # Install ZED
+    if $ZED || $ALL ; then
+        zed
+    fi
     # Install ROS
     if $ROS || $ALL ; then
         ros $DISTRO
@@ -256,7 +325,7 @@ main()
     echo "---------------------------"
     echo " Installed:"
     list_components
-    if $REQUIRE_REBOOT ; then
+    if [ -f /var/run/reboot-required ] ; then
         # After install require reboot
         echo "${red}Require reboot${reset}"
     fi
