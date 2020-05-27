24412378e608618b6478a01e29055799ad4ab3be
Raffaello Bonghi <raffaello@rnext.it>
2020-05-27 18:56:14 +0100

    Improve opencv installer

--- opencv.sh
+++ opencv.sh
@@ -30,9 +30,11 @@ green=`tput setaf 2`
 yellow=`tput setaf 3`
 reset=`tput sgr0`
 
-
+INSTALL_OPENCV_PATH="/usr/local"
 INSTALL_OPENCV_CONTRIB="YES"
 INSTALL_OPENCV_EXTRA="YES"
+INSTALL_OPENCV_PYTHON2="YES"
+INSTALL_OPENCV_PYTHON3="YES"
 
 
 opencv_installer()
@@ -43,14 +45,22 @@ opencv_installer()
     local OPENCV_VERSION=$1
     local BUILD_FOLDER=$2
     
-    echo "Install dependencies for $OPENCV_VERSION"
+    echo "${green}Install dependencies for $OPENCV_VERSION${reset}"
     # Install dependencies
-    sudo apt-get install -y cmake pkg-config \
-        libavcodec-dev libavformat-dev libavutil-dev libeigen3-dev \
-        libglew-dev libgtk2.0-dev libgtk-3-dev libjpeg-dev libpng-dev \
-        libpostproc-dev libswscale-dev libtbb-dev libtiff5-dev \
-        libv4l-dev libxvidcore-dev libx264-dev qt5-default zlib1g-dev \
-        libgl1 libglvnd-dev
+    sudo apt-get install -y build-essential cmake git gfortran pkg-config \
+        libatlas-base-dev libavcodec-dev libavformat-dev libavresample-dev libgtk-3-dev \
+        libcanberra-gtk3-module libdc1394-22-dev libeigen3-dev libglew-dev \
+        libgstreamer-plugins-base1.0-dev libgstreamer-plugins-good1.0-dev libgstreamer1.0-dev \
+        libjpeg-dev libjpeg8-dev libjpeg-turbo8-dev liblapack-dev liblapacke-dev \
+        libopenblas-dev libpng-dev libpostproc-dev libswscale-dev libtbb-dev libtbb2 \
+        libtesseract-dev libtiff-dev libv4l-dev libxine2-dev libxvidcore-dev libx264-dev \
+        qv4l2 v4l-utils v4l2ucp zlib1g-dev
+    if [ $INSTALL_OPENCV_PYTHON2 == "YES" ] ; then
+        sudo apt-get install -y python-dev python-numpy
+    fi
+    if [ $INSTALL_OPENCV_PYTHON3 == "YES" ] ; then
+        sudo apt-get install -y python3-dev python3-numpy python3-matplotlib
+    fi
 
     ### Download last stable opencv source code
     echo "${green}Download OpenCV $OPENCV_VERSION source code in $opencv_source_path${reset}"
@@ -77,9 +87,6 @@ opencv_installer()
     fi
         
     echo "Build openCV $JETSON_DESCRIPTION for $JETSON_DESCRIPTION ${bold}($JETSON_CUDA_ARCH_BIN)${reset}"
-
-    cd $BUILD_FOLDER/opencv
-    
     if [ -d "build" ] ; then
         echo "${yellow}Clean old Build folder${reset}"
         rm -R build
@@ -87,27 +94,51 @@ opencv_installer()
     mkdir build 
     cd build
 
-    # Add extra options for OpenCV contrib
-    local opencv_contrib_option=""
-    if [ $INSTALL_OPENCV_CONTRIB == "YES" ] ; then
-        opencv_contrib_option="-D OPENCV_EXTRA_MODULES_PATH=../opencv_contrib/modules"
-    fi
-    
-    # Add extra options for OpenCV extras
-    local opencv_extras_option=""
-    if [ $INSTALL_OPENCV_EXTRA == "YES" ] ; then
-        opencv_extras_option="-D INSTALL_TESTS=ON -D OPENCV_TEST_DATA_PATH=../opencv_extra/testdata"
-    fi
-
     # CMAKE
-    time cmake -D CMAKE_BUILD_TYPE=RELEASE -D CMAKE_INSTALL_PREFIX=$PATCH_OPENCV_INSTALL_PATH \
-          -D WITH_CUDA=ON -D CUDA_ARCH_BIN=$JETSON_CUDA_ARCH_BIN -D CUDA_ARCH_PTX="" \
-          -D WITH_CUBLAS=ON -D ENABLE_FAST_MATH=ON -D CUDA_FAST_MATH=ON \
-          -D ENABLE_NEON=ON -D WITH_LIBV4L=ON -D BUILD_TESTS=OFF \
-          -D BUILD_PERF_TESTS=OFF -D BUILD_EXAMPLES=OFF \
-          -D WITH_GSTREAMER=ON -D WITH_GSTREAMER_0_10=OFF \
-          -D BUILD_opencv_python2=ON -D BUILD_opencv_python3=ON \
-          -D WITH_QT=ON -D WITH_OPENGL=ON $opencv_xavier_option $opencv_contrib_option $opencv_extras_option ..
+    local CMAKEFLAGS="
+        -D CMAKE_BUILD_TYPE=RELEASE
+        -D CMAKE_INSTALL_PREFIX=$INSTALL_OPENCV_PATH
+        -D WITH_CUDA=ON
+        -D CUDA_ARCH_BIN=$JETSON_CUDA_ARCH_BIN
+        -D CUDA_ARCH_PTX=''
+        -D WITH_CUBLAS=ON
+        -D ENABLE_FAST_MATH=ON
+        -D CUDA_FAST_MATH=ON
+        -D ENABLE_NEON=ON
+        -D WITH_LIBV4L=ON
+        -D BUILD_TESTS=OFF
+        -D BUILD_PERF_TESTS=OFF
+        -D BUILD_EXAMPLES=OFF
+        -D WITH_GSTREAMER=ON
+        -D WITH_GSTREAMER_0_10=OFF
+        -D WITH_QT=ON
+        -D WITH_OPENGL=ON
+        -D EIGEN_INCLUDE_PATH=/usr/include/eigen3
+        -D WITH_GSTREAMER=ON
+        -D OPENCV_GENERATE_PKGCONFIG=ON"
+        # TODO: Check "-D OPENCV_ENABLE_NONFREE=ON"
+        if [ $INSTALL_OPENCV_CUDNN == "YES" ] ; then
+            CMAKEFLAGS=$CMAKEFLAGS+"-D WITH_CUDNN=ON
+                                    -D OPENCV_DNN_CUDA=ON
+                                    -D CUDNN_VERSION='8.0'"
+        fi
+        if [ $INSTALL_OPENCV_PYTHON2 == "YES" ] ; then
+            CMAKEFLAGS=$CMAKEFLAGS+"-D BUILD_opencv_python2=ON"
+        fi
+        if [ $INSTALL_OPENCV_PYTHON3 == "YES" ] ; then
+            CMAKEFLAGS=$CMAKEFLAGS+"-D BUILD_opencv_python3=ON"
+        fi
+        if [ $INSTALL_OPENCV_CONTRIB == "YES" ] ; then
+            CMAKEFLAGS=$CMAKEFLAGS+"-D OPENCV_EXTRA_MODULES_PATH=../opencv_contrib/modules"
+        fi
+        if [ $INSTALL_OPENCV_EXTRA == "YES" ] ; then
+            CMAKEFLAGS=$CMAKEFLAGS+"-D INSTALL_TESTS=ON
+                                    -D OPENCV_TEST_DATA_PATH=../opencv_extra/testdata"
+        fi
+    echo $CMAKEFLAGS
+    exit 0
+
+    time cmake $CMAKEFLAGS
 
     if [ $? -eq 0 ] ; then
         echo "CMake configuration make successful"
@@ -186,7 +217,7 @@ main()
     done
 
     # Check Jetson board
-    if [ -z $JETSON_DESCRIPTION ] ; then
+    if [ -z "$JETSON_MACHINE" ] ; then
         echo "${red}jetson-stats not installed${reset}"
         exit 1
     fi
@@ -213,8 +244,8 @@ main()
 
     # Make info table
     echo "----- Installation -----"
-    echo "$JETSON_DESCRIPTION [$JETSON_L4T]"
-    echo "$JETSON_CUDA (ARCH BIN) $JETSON_CUDA_ARCH_BIN"
+    echo "$JETSON_MACHINE [$JETSON_L4T]"
+    echo "CUDA $JETSON_CUDA (ARCH BIN) $JETSON_CUDA_ARCH_BIN"
     echo "cuDNN $JETSON_CUDNN"
     # OpenCV version
     if [ $JETSON_OPENCV != $OPENCV_VERSION ] ; then
